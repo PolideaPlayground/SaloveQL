a154840609ad803bb890d737da42895e11301217
Piotr Dubiel <piotr.dubiel@polidea.com>
2017-08-25 19:47:17 +0200

    CORS

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import schema from './schema'
@@ -12,6 +13,21 @@ const executableSchema = makeExecutableSchema({
     typeDefs: schema,
     resolvers: resolvers,
 })
+
+const corsOptions = {
+    origin(origin, callback) {
+        callback(null, true);
+    },
+    credentials: true
+};
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
+app.use(cors(corsOptions))
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: executableSchema }));
 
